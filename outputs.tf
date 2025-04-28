14fc670444e63f5ccb7d7c6a4cc68d871c59ab20
Oscar Ivan <o@maleficarum.mx>
2025-04-28 14:27:28 -0600

    Add basic structure

---
+++ outputs.tf
@@ -0,0 +1,6 @@
+
+# tflint-ignore: terraform_unused_declarations
+output "ad" {
+  value = data.oci_identity_availability_domains.oci_identity_availability_domain
+  description = "AD"
+}
---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "vcn_definition" {
+    description = "VCN Definition"
+    type = object({
+      cidr_block = string,
+      name = string
+    })
+}
+
+variable "private_subnet_definition" {
+    description = "Private subnet Definition"
+    type = object({
+      cidr_block = string,
+      name = string
+    })
+}
+
+variable "public_subnet_definition" {
+    description = "Public subnet Definition"
+    type = object({
+      cidr_block = string,
+      name = string
+    })
+}
+
+variable "compartment_id" {
+    type = string
+    description = "Compartment to deploy"
+}
