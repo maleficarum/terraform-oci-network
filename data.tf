13f0fb58d9209440460dc86bb890416f30fbae6c
Oscar Ivan <o@maleficarum.mx>
2025-04-29 16:50:13 -0600

    Updated data and variables to include availability domains

--- data.tf
+++ data.tf
@@ -1,5 +1,4 @@
 # tflint-ignore: terraform_unused_declarations
 data "oci_identity_availability_domains" "oci_identity_availability_domain" {
-  #Required
-  compartment_id = "ocid1.tenancy.oc1..aaaaaaaal44x67fnxtof4bplqqdidhii5mzx5sd7x4a6hgq2pvmjcpvszura"
+  compartment_id = var.compartment_id
 }
