cd0d38933e59b99f8da6ab7aac89acc6f2e5eb7f
Oscar Ivan <o@maleficarum.mx>
2025-04-28 20:47:41 -0600

    Adjusted parameters on subnets to fix non-connection issues

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 
 # tflint-ignore: terraform_unused_declarations
 output "ad" {
-  value = data.oci_identity_availability_domains.oci_identity_availability_domain
+  value       = data.oci_identity_availability_domains.oci_identity_availability_domain
   description = "AD"
 }
--- variables.tf
+++ variables.tf
@@ -1,28 +1,29 @@
 variable "vcn_definition" {
-    description = "VCN Definition"
-    type = object({
-      cidr_block = string,
-      name = string
-    })
+  description = "VCN Definition"
+  type = object({
+    cidr_block = string,
+    name       = string,
+    dbs_label  = string
+  })
 }
 
 variable "private_subnet_definition" {
-    description = "Private subnet Definition"
-    type = object({
-      cidr_block = string,
-      name = string
-    })
+  description = "Private subnet Definition"
+  type = object({
+    cidr_block = string,
+    name       = string
+  })
 }
 
 variable "public_subnet_definition" {
-    description = "Public subnet Definition"
-    type = object({
-      cidr_block = string,
-      name = string
-    })
+  description = "Public subnet Definition"
+  type = object({
+    cidr_block = string,
+    name       = string
+  })
 }
 
 variable "compartment_id" {
-    type = string
-    description = "Compartment to deploy"
+  type        = string
+  description = "Compartment to deploy"
 }
