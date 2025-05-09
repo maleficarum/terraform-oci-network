2702287d40dc1709bc80835f70a76665a91643b0
Oscar Ivan <o@maleficarum.mx>
2025-05-08 22:51:47 -0600

    feat:
    - Added service gateway.
    - Added egress rule to allow LB communication with OKE nodes.
    - Added redis ingress rule.
    - fix: Added rule to allow workers to fetch certificates from API server on provisioning.

--- data.tf
+++ data.tf
@@ -6,10 +6,18 @@ data "oci_identity_availability_domains" "oci_identity_availability_domain" {
 data "oci_core_images" "oke_worker_images" {
   compartment_id           = var.compartment_id
   operating_system         = "Oracle Linux"
-  operating_system_version = "8"  # or "7", "9"
+  operating_system_version = "8" # or "7", "9"
   filter {
     name   = "display_name"
     values = ["^Oracle-Linux-.*-OKE-.*"]
+    regex  = true
+  }
+}
+
+data "oci_core_services" "all_services" {
+  filter {
+    name   = "name"
+    values = ["All .* Services In Oracle Services Network"]
     regex  = true
   }
 }
