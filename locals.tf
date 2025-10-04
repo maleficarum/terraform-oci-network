a951265b9b0558071615888e8a55a0b5252efd14
Oscar Ivan <o@maleficarum.mx>
2025-10-04 10:47:23 -0600

    Fix/resolver (#41)
    * feat(All): Export network information as output
    
    BREAKING CHANGE:
    
    * feat: Allog multiple subnet creation
    
    Allog multiple subnet creation for private and public subnets (not only one)
    
    BREAKING CHANGE:
    
    * feat(All): Add support to use a preexisting comparment
    
    BREAKING CHANGE: Closes #38
    
    * feat(Network): Add support to use existent compartment
    
    BREAKING CHANGE:

--- locals.tf
+++ locals.tf
@@ -1,8 +1,8 @@
 locals {
   # Determine compartment ID with validation
   compartment_id = (
-    var.existing_compartment != "" ? 
-    var.existing_compartment : 
+    var.existing_compartment != "" ?
+    var.existing_compartment :
     (length(oci_identity_compartment.network_compartment) > 0 ? oci_identity_compartment.network_compartment[0].id : var.compartment_id)
   )
 }
