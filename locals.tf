c9fae17aee7b8553b2a00217c859d7a131fee77d
Malleus Malegicarum <o@maleficarum.mx>
2025-10-29 23:20:00 -0600

    feat(Security): Add security rules
    Add more specific security rules params.
    
    BREAKING CHANGE:

--- locals.tf
+++ locals.tf
@@ -5,4 +5,8 @@ locals {
     var.existing_compartment :
     (length(oci_identity_compartment.network_compartment) > 0 ? oci_identity_compartment.network_compartment[0].id : var.compartment_id)
   )
+  # security_list_ids_by_subnet_name = {
+  #   for sl in var.private_security_rules :
+  #   sl.subnetwork_name => oci_core_security_list.private_security_list[index(var.private_security_rules, sl)].id
+  # }
 }
